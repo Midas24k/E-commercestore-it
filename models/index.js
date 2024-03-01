c6afe1cce66c50e37c399c1ab2f2fe1ee08e9a4c
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-03-01 10:44:56 -0500

    finished all relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'catagory_id,'
+});
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsToMany(Tag, {
+  foreignKey:"tag_id",
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey:'product_id',
+  onDelete: 'CASCADE',
+});
+
+
+
+
 
 module.exports = {
   Product,
