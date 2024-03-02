85860585b7a2f306c2d98973e113d3929a61bde8
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-03-01 19:55:42 -0500

    started the Product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include:[Category, Tag]
+      // be sure to include its associated Category and Tag data
+      
+    })
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log("Error", err)
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
+  
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
